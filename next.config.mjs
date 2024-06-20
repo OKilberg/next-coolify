26a77d68bca78b8a385b28253153bdf841a87621
OKilberg <oscar.oravakilberg@gmail.com>
2024-06-20 13:13:52 +0200

    static build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export'
+};
 
 export default withPayload(withPayload(withPayload(nextConfig)));
